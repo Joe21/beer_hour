37e822c5b3c7ee858ecba7f55ed30e342e230bb9
francescawedemeyer <flwedemeyer@gmail.com>
2013-11-01 16:49:27 -0400

    Splash page

--- beer_app/app/controllers/beer_controller.rb
+++ beer_app/app/controllers/beer_controller.rb
@@ -1,5 +1,9 @@
 class BeerController < ApplicationController
 
+	def index
+
+	end	
+
 	def beer_feed
 		ig_client_id = ENV['INSTAGRAM_CLIENT_ID']
 		url = "https://api.instagram.com/v1/tags/beerporn/media/recent?client_id=#{ig_client_id}"
