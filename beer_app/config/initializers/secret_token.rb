4ba8aa83578e545302d8caabdc1615a7779e2dc0
Joe21 <joej21@gmail.com>
2013-11-01 14:32:39 -0400

    Pre bundle

---
+++ beer_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeerApp::Application.config.secret_key_base = 'bd7f2b95858c5d88562f2c96fec982c494ee3f1d99e8c25fdd51678c8409d9403388eebb8f219ed00abee893a58dae6b5c3a127a01457c3389c749b819943191'
