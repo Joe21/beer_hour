ee83f958e9e9111de1725c6009c4d159353b418c
Joe21 <joej21@gmail.com>
2013-11-01 17:15:18 -0400

    mvp

--- beer_app/app/controllers/beer_controller.rb
+++ beer_app/app/controllers/beer_controller.rb
@@ -2,9 +2,14 @@ class BeerController < ApplicationController
 
 	def index
 
+		ig_client_id = ENV['INSTAGRAM_CLIENT_ID']
+		url = "https://api.instagram.com/v1/tags/beerporn/media/recent?client_id=#{ig_client_id}"
+		@response = HTTParty.get(url)
+		render :json => @response
 	end	
 
 	def beer_feed
+		puts params
 		ig_client_id = ENV['INSTAGRAM_CLIENT_ID']
 		url = "https://api.instagram.com/v1/tags/beerporn/media/recent?client_id=#{ig_client_id}"
 		@response = HTTParty.get(url)
