899138f260a2f8c83f562eea9c77e2d29fba6798
Joe21 <joej21@gmail.com>
2013-11-02 11:15:52 -0400

    saturday morning

--- beer_app/app/controllers/beer_controller.rb
+++ beer_app/app/controllers/beer_controller.rb
@@ -1,11 +1,6 @@
 class BeerController < ApplicationController
 
 	def index
-
-		ig_client_id = ENV['INSTAGRAM_CLIENT_ID']
-		url = "https://api.instagram.com/v1/tags/beerporn/media/recent?client_id=#{ig_client_id}"
-		@response = HTTParty.get(url)
-		render :json => @response
 	end	
 
 	def beer_feed
